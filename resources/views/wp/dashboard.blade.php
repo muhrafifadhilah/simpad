bd1a0e4c6bd074d511c2a71fa1dc5966e158e14d
Reza Pratama <prezaa971@gmail.com>
2025-07-10 08:01:33 +0700

    done

--- resources/views/wp/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<div class="container">
-    <h4>Executive Summary Wajib Pajak</h4>
-    <div class="alert alert-info">Selamat datang, {{ Auth::user()->name }}!</div>
-    <a href="{{ route('wp.sptpd') }}" class="btn btn-primary">Lihat Data SPTPD Saya</a>
-</div>
-@endsection
